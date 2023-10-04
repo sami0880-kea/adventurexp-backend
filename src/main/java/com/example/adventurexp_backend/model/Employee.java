de90dbe3ec71d3664de145d2c2c689e659fa04fa
Nichlas <nich5384@stud.kea.dk>
2023-10-04 12:48:31 +0200

    tilføjet jnuit dependency employee samt repository, så der er klar til login system

---
+++ src/main/java/com/example/adventurexp_backend/model/Employee.java
@@ -0,0 +1,22 @@
+package com.example.adventurexp_backend.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String password;
+
+}
