ddb28e01bd157d20157d32ed3e87020e12ee0c27
Filip Gynthersen <filipg@live.dk>
2023-10-04 11:33:38 +0200

    add equipment class

---
+++ src/main/java/com/example/adventurexp_backend/model/Equipment.java
@@ -0,0 +1,24 @@
+package com.example.adventurexp_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Equipment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int equipmentId;
+    private String equipmentName;
+    private int equipmentAmountId;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private Activity activity;
+}
