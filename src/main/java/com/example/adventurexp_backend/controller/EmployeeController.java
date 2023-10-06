895b252e5268cdb7122bb5ee131258a5cf870a43
Nichlas <nich5384@stud.kea.dk>
2023-10-06 11:54:24 +0200

    Lavet getEmployeeById kald i controllerklassen

--- src/main/java/com/example/adventurexp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/adventurexp_backend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.adventurexp_backend.controller;
 
+import com.example.adventurexp_backend.dto.EmployeeDTO;
 import com.example.adventurexp_backend.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class EmployeeController {
     @Autowired
-    EmployeeService EmployeeService;
+    EmployeeService employeeService;
+
+    @GetMapping ("/employee/{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable("id") int id) {
+        EmployeeDTO employeeDTO = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employeeDTO);
+    }
 
 }
