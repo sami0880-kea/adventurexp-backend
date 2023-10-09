02d221213479bbd1db46432b2ad8bf781a975bbd
Filip Gynthersen <filipg@live.dk>
2023-10-09 15:29:28 +0200

    EquipmentService+Repo changes

--- src/main/java/com/example/adventurexp_backend/service/EquipmentService.java
+++ src/main/java/com/example/adventurexp_backend/service/EquipmentService.java
@@ -8,7 +8,9 @@ import com.example.adventurexp_backend.repository.EquipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class EquipmentService {
@@ -26,13 +28,37 @@ public class EquipmentService {
     public EquipmentDTO updateEquipment(int id, EquipmentDTO equipmentDTO) {
         Optional<Equipment> existingEquipment = equipmentRepository.findById(id);
         if(existingEquipment.isPresent()) {
-            Equipment equipmentToupdate = equipmentConverter.toEntity(equipmentDTO);
-            equipmentToupdate.setId(id);
-            Equipment savedEquipment = equipmentRepository.save(equipmentToupdate);
+            Equipment equipmentToUpdate = equipmentConverter.toEntity(equipmentDTO);
+            equipmentToUpdate.setId(id);
+            Equipment savedEquipment = equipmentRepository.save(equipmentToUpdate);
             return equipmentConverter.toDTO(savedEquipment);
         } else {
             throw new NotFoundException("Equipment not found with id: " +id);
         }
     }
+
+    public List<EquipmentDTO> findAllEquipmentByActivityId(int activityId) {
+        List<Equipment> equipment = equipmentRepository.findAllByActivityId(activityId);
+        return equipment.stream()
+                .map(equipmentConverter::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteEquipmentById(int id) {
+        Optional<Equipment> equipment = equipmentRepository.findById(id);
+        if(equipment.isPresent()) {
+            equipmentRepository.deleteById(id);
+        } else {
+            throw new NotFoundException("Equipment not found with id:" +id);
+        }
+    }
+
+    public EquipmentDTO createEquipment(EquipmentDTO equipmentDTO) {
+        Equipment equipmentToSave = equipmentConverter.toEntity(equipmentDTO);
+        equipmentToSave.setId(0);
+        Equipment savedEquipment = equipmentRepository.save(equipmentToSave);
+        return equipmentConverter.toDTO(savedEquipment);
+    }
+
 
 }
